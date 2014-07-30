cecf80bde02bab01a2bae7be91a1dc5b2ed8cc40
djimd <jimder2014@gmail.com>
2014-07-29 23:07:20 -0700

    fth

---
+++ src/ca/bcit/comp2613/a00115507/fth/CharacterRepository.java
@@ -0,0 +1,11 @@
+package ca.bcit.comp2613.a00115507.fth;
+
+
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+
+
+
+public interface CharacterRepository extends CrudRepository<Character, String> {
+
+}
---
+++ src/ca/bcit/comp2613/a00115507/fth/InvalidDirectoryException.java
@@ -0,0 +1,12 @@
+package ca.bcit.comp2613.a00115507.fth;
+
+public class InvalidDirectoryException extends Exception {
+	/**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    public InvalidDirectoryException(String str) {
+		super(str);
+	}
+}
